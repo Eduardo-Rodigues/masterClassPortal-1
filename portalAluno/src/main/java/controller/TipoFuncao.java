2942bc51a69a1ec255d9a5c7049b2d12c15ab008
Eduardo Rodrigues <edurodrigues188@gmail.com>
2023-05-09 20:53:37 -0300

    Update TipoFuncao.java
    Uptade e criado os métodos pertinentes ao diagrama.

--- portalAluno/src/main/java/controller/TipoFuncao.java
+++ portalAluno/src/main/java/controller/TipoFuncao.java
@@ -5,21 +5,62 @@
  */
 package controller;
 
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author aluno
  */
 public class TipoFuncao {
-    public String nomefuncao;
-    public int codigofuncao;
-    public String setorfuncao;
-    
-    public static void cadastrarFuncao(String[] args) {
-        System.out.println("Em Desenvolvimento");
-        Menu.main(args);
-
+    private ArrayList<String> nomeFuncao = new ArrayList<>();
+    // adição de método useLocale() para tratamento de caraçteres da lingua portuguesa.
+    public void cadastrarFuncao() {
+        Scanner lerInput = new Scanner(System.in).useLocale(new Locale("pt", "BR"));
+        System.out.print("Digite o nome da função: ");
+        String nome = lerInput.nextLine();
+        nomeFuncao.add(nome);
+        System.out.println("Função cadastrada com sucesso!");
     }
-    public void cadastrarSetor() {
 
-    } 
+    public void editarFuncao() {
+        Scanner lerInput = new Scanner(System.in);
+        System.out.print("Digite o código da função que deseja editar: \nSe não souber vá até a opção Exibir Função. ");
+        int indice = lerInput.nextInt();
+        lerInput.nextLine(); // Consumir a quebra de linha
+        if (indice >= 0 && indice < nomeFuncao.size()) {
+            System.out.print("Digite o novo nome da função: ");
+            String nome = lerInput.nextLine();
+            nomeFuncao.set(indice, nome);
+            System.out.println("Função editada com sucesso!");
+        } else {
+            System.out.println("Índice inválido!");
+        }
+    }
+
+    public void excluirFuncao() {
+        Scanner lerInput = new Scanner(System.in);
+        System.out.print("Digite o código da função que deseja excluir: ");
+        int indice = lerInput.nextInt();
+        lerInput.nextLine(); // Consumir a quebra de linha
+        if (indice >= 0 && indice < nomeFuncao.size()) {
+            nomeFuncao.remove(indice);
+            System.out.println("Função excluída com sucesso!");
+        } else {
+            System.out.println("Código inválido!");
+        }
+    }
+
+    public String exibirFuncao() {
+        if (nomeFuncao.isEmpty()) {
+            return "Nenhuma função cadastrada!";
+        } else {
+        String resultado = "Funções cadastradas:\n";
+        for (int i = 0; i < nomeFuncao.size(); i++) {
+            resultado += i + ": " + nomeFuncao.get(i) + "\n";
+        }
+        return resultado;
+        }
+    }
 }
