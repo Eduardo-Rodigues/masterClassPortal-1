7bda1f07682e1398d4cc11429ccbdaa296ff73c9
Fernanda Hallmann <111521249+FernandaHallmann@users.noreply.github.com>
2023-05-10 16:08:49 -0300

    Add files via upload

--- portalAluno/src/main/java/controller/Instituicao.java
+++ portalAluno/src/main/java/controller/Instituicao.java
@@ -2,66 +2,62 @@
 Analista Responsável:  Bruno
 Desenvolvedor que criou: João Contini
 Desenvolvedor que assumiu: Fernanda
-*/
-
+ */
 package controller;
 
 import java.util.Scanner;
 
 public class Instituicao {
-    
-    public static void validaSetores(String[] main){
+
+    public static void validaSetores(String[] main) {
+        Scanner lerSetor = new Scanner(System.in);
+        
         boolean aluno = false;
         boolean financeiro = false;
         boolean curso = false;
-        boolean logradouro = false;
         boolean biblioteca = false;
         boolean juridico = false;
-        Scanner ler = new Scanner(System.in);
-        String setor;
-        setor = ler.next();
-        switch (setor) {
+        String nomeDoSetor;
+        String existeSetor = "Esse setor existe dentro desta instituição";
+        String naoExisteSetor = "Esse setor não existe dentro desta instituição";
+
+        System.out.print("Informe o setor: ");
+        nomeDoSetor = lerSetor.next();
+        switch (nomeDoSetor) {
             case "aluno":
                 if (aluno == true) {
-                    System.out.println("Esse setor existe dentro desta instituição");
+                    System.out.println(existeSetor);
                 } else {
-                    System.out.println("Esse setor não existe dentro desta instituição");
+                    System.out.println(naoExisteSetor);
                 }
                 break;
             case "financeiro":
                 if (financeiro == true) {
-                    System.out.println("Esse setor existe dentro desta instituição");
+                    System.out.println(existeSetor);
                 } else {
-                    System.out.println("Esse setor não existe dentro desta instituição");
+                    System.out.println(naoExisteSetor);
                 }
                 break;
             case "curso":
                 if (curso == true) {
-                    System.out.println("Esse setor existe dentro desta instituição");
+                    System.out.println(existeSetor);
                 } else {
-                    System.out.println("Esse setor não existe dentro desta instituição");
-                }
-                break;
-            case "logradouro":
-                if (logradouro == true) {
-                    System.out.println("Esse setor existe dentro desta instituição");
-                } else {
-                    System.out.println("Esse setor não existe dentro desta instituição");
+                    System.out.println(naoExisteSetor);
                 }
                 break;
             case "biblioteca":
                 if (biblioteca == true) {
-                    System.out.println("Esse setor existe dentro desta instituição");
+                    System.out.println(existeSetor);
                 } else {
-                    System.out.println("Esse setor não existe dentro desta instituição");
+                    System.out.println(naoExisteSetor);
                 }
                 break;
             case "juridico":
                 if (juridico == true) {
-                    System.out.println("Esse setor existe dentro desta instituição");
+                    System.out.println(existeSetor);
                 } else {
-                    System.out.println("Esse setor não existe dentro desta instituição");
-                }         
+                    System.out.println(naoExisteSetor);
+                }
                 break;
             default:
                 System.out.println("O setor informado não existe no corpo acadêmico desta instituição");
