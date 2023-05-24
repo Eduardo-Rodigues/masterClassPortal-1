7f3630d6f9153c36680247bbcba1e345a26b6477
Eduardo Rodrigues <edurodrigues188@gmail.com>
2023-05-24 19:22:11 -0300

    Create Reitor.java
    Criação da classe Reitor, a qual herda de Funcionário, que or sua vez herda de Pessoa

---
+++ portalAluno/src/main/java/controller/Reitor.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.Date;
+
+/**
+ *
+ * @author eduro
+ */
+public class Reitor extends Funcionario {
+    private String departamento;
+
+    public Reitor(String nomeSobrenome, String CPF, int registroGeral, String tipoFuncao, Date dataNasc, Date dataRegistro, String email, String telefone, boolean pcd, String genero, String endereco, String cargo, double salario, String departamento) {
+        super(nomeSobrenome, CPF, registroGeral, tipoFuncao, dataNasc, dataRegistro, email, telefone, pcd, genero, endereco, cargo, salario);
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public void editar(String nomeSobrenome, String CPF, int registroGeral, String tipoFuncao, Date dataNasc, Date dataRegistro, String email, String telefone, boolean pcd, String genero, String endereco, String cargo, double salario) {
+        super.editar(nomeSobrenome, CPF, registroGeral, tipoFuncao, dataNasc, dataRegistro, email, telefone, pcd, genero, endereco, cargo, salario);
+    }
+
+    @Override
+    public void excluirPessoa() {
+        super.excluirPessoa();
+        this.departamento = null;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", departamento='" + departamento + '\'' +
+                '}';
+    }
+}
