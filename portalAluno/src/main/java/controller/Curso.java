71afe900dce97d7062c10ac1a8be7be393040ca4
Eduardo Rodrigues <edurodrigues188@gmail.com>
2023-05-24 21:16:24 -0300

    Update Curso.java

--- portalAluno/src/main/java/controller/Curso.java
+++ portalAluno/src/main/java/controller/Curso.java
@@ -15,144 +15,115 @@ public class Curso {
     private String disciplina;
     private String turnoCurso;
     private String campusAcademico;
-    ArrayList<Cadastro> cadastro;
+    private ArrayList<Cadastro> cadastro;
 
     public Curso() {
-        cadastro = new ArrayList<Cadastro>();
+        cadastro = new ArrayList<>();
     }
 
     public void cadastrarCurso() {
-        String disci;
-        String turno;
-        String campus;
-        int carga;
+        Scanner scanner = new Scanner(System.in);
+
         boolean cadastrar = true;
 
-        try {
+        while (cadastrar) {
+            System.out.println("Informe a disciplina do curso: ");
+            String disci = scanner.nextLine();
 
-            while (cadastrar) {
-                System.out.println("Informe a disciplina do curso: ");
-                Scanner lerDisci = new Scanner(System.in);
-                disci = lerDisci.nextLine();
+            System.out.println("Informe qual o turno do curso: ");
+            String turno = scanner.nextLine();
 
-                System.out.println("Informe qual o turno do curso: ");
-                Scanner lerTurno = new Scanner(System.in);
-                turno = lerTurno.nextLine();
+            System.out.println("Informe qual é o campus do curso: ");
+            String campus = scanner.nextLine();
 
-                System.out.println("Informe qual é o campus do curso: ");
-                Scanner lerCampus = new Scanner(System.in);
-                campus = lerCampus.nextLine();
+            System.out.println("Informe a carga horaria do curso: ");
+            int carga = scanner.nextInt();
+            scanner.nextLine();
 
-                System.out.println("Informe a  carga horaria do curso: ");
-                Scanner lerCarga = new Scanner(System.in);
-                carga = lerCarga.nextInt();
+            cadastro.add(new Cadastro(campus, disci, turno, carga));
 
-                cadastro.add(new Cadastro(campus, disci, turno, carga));
-
-                for (int i = 0; i < cadastro.size(); i++) {
-                    Cadastro listaCurso = cadastro.get(i);
-                    System.out.println("\n========= Curso cadastrado com Sucesso!===========");
-                    System.out.println("Curso: " + listaCurso.getDisciplina() + " - " + listaCurso.getTurno() + " - " + listaCurso.getCampus() + " - " + listaCurso.getCargaHoraria());
-                }
-
-                System.out.println("\nDeseja cadastrar mais um Curso? (S/N)");
-                Scanner lerScanner = new Scanner(System.in);
-                String resposta = lerScanner.nextLine();
-
-                if (resposta.equalsIgnoreCase("S")) {
-                    cadastrar = true;
-                } else {
-                    cadastrar = false;
-                }
+            for (Cadastro curso : cadastro) {
+                System.out.println("\n========= Curso cadastrado com Sucesso!===========");
+                System.out.println("Curso: " + curso.getDisciplina() + " - " + curso.getTurno() + " - " + curso.getCampus() + " - " + curso.getCargaHoraria());
             }
 
-        } catch (Exception exception) {
-            System.out.println("ERRO! Informe os dados corretamente");
-            System.exit(1);
+            System.out.println("\nDeseja cadastrar mais um Curso? (S/N)");
+            String resposta = scanner.nextLine();
+
+            if (!resposta.equalsIgnoreCase("S")) {
+                cadastrar = false;
+            }
         }
     }
 
     public void removerCurso() {
-        Scanner ler = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        if (this.cadastro.size() >= 0) {
+        if (!cadastro.isEmpty()) {
             System.out.println("\n============== Cursos Cadastrados: ==============\n");
             for (Cadastro curso : cadastro) {
                 System.out.println(curso.getDisciplina() + " - " + curso.getTurno());
             }
         } else {
-            System.out.println("Nenhum curso Cadastrado");
-            System.exit(1);
+            System.out.println("Nenhum curso cadastrado");
+            return;
         }
 
         System.out.println("\nInforme qual curso você deseja remover: ");
-        String curso = ler.nextLine();
+        String curso = scanner.nextLine();
 
         for (Cadastro cadastros : cadastro) {
             if (cadastros.getDisciplina().equals(curso)) {
-                int i = cadastro.indexOf(cadastros);
-                cadastro.remove(i);
+                cadastro.remove(cadastros);
                 System.out.println("\nCurso Removido: " + cadastros.getDisciplina());
                 return;
-            } else {
-                System.out.println("\nCurso não encontrado");
-                System.exit(1);
             }
         }
+
+        System.out.println("\nCurso não encontrado");
     }
 
     public void editarCurso() {
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-
-            if (this.cadastro.size() >= 0) {
-                System.out.println("\n============== Cursos Cadastrados: ==============\n");
-                for (Cadastro curso : cadastro) {
-                    System.out.println(curso.getDisciplina() + " - " + curso.getTurno());
-                }
-
-            } else {
-                System.out.println("Nenhuma curso cadastrada");
-                System.exit(1);
+        if (!cadastro.isEmpty()) {
+            System.out.println("\n============== Cursos Cadastrados: ==============\n");
+            for (Cadastro curso : cadastro) {
+                System.out.println(curso.getDisciplina() + " - " + curso.getTurno());
             }
-
-            System.out.println("\nInforme qual curso que deseja editar: ");
-            Scanner ler = new Scanner(System.in);
-            String editar = ler.nextLine();
-
-            for (Cadastro listaCurso : cadastro) {
-                if (listaCurso.getDisciplina().equals(editar)) {
-                    int i = cadastro.indexOf(listaCurso);
-                    cadastro.remove(i);
-                }
-            }
-
-            System.out.println("Informe as novas informações do curso: \n");
-
-            System.out.println("Informe a disciplina do curso: ");
-            Scanner lerTurmaEditado = new Scanner(System.in);
-            String disciplinaEditadaCurso = lerTurmaEditado.nextLine();
-
-            System.out.println("Informe o turno do curso: ");
-            Scanner lerTurnoEditado = new Scanner(System.in);
-            String turnoEditadoCurso = lerTurnoEditado.nextLine();
-
-            System.out.println("Informe o campus: ");
-            Scanner lerCampusEditado = new Scanner(System.in);
-            String campusEditadoCurso = lerCampusEditado.nextLine();
-
-            System.out.println("Informe qual a carga horária: ");
-            Scanner cargaHorariaEditadaCurso = new Scanner(System.in);
-            int cargaHorariaEditada = cargaHorariaEditadaCurso.nextInt();
-
-            cadastro.add(new Cadastro(campusEditadoCurso, disciplinaEditadaCurso, turnoEditadoCurso, cargaHorariaEditada));
-
-            System.out.println("========= Turma editada com Sucesso!===========");
-
-        } catch (Exception exception) {
-            System.out.println("ERRO! Informe os dados corretamente");
-            System.exit(1);
+        } else {
+            System.out.println("Nenhuma curso cadastrada");
+            return;
         }
+
+        System.out.println("\nInforme qual curso deseja editar: ");
+        String editar = scanner.nextLine();
+
+        for (Cadastro listaCurso : cadastro) {
+            if (listaCurso.getDisciplina().equals(editar)) {
+                cadastro.remove(listaCurso);
+                break;
+            }
+        }
+
+        System.out.println("Informe as novas informações do curso: \n");
+
+        System.out.println("Informe a disciplina do curso: ");
+        String disciplinaEditadaCurso = scanner.nextLine();
+
+        System.out.println("Informe o turno do curso: ");
+        String turnoEditadoCurso = scanner.nextLine();
+
+        System.out.println("Informe o campus: ");
+        String campusEditadoCurso = scanner.nextLine();
+
+        System.out.println("Informe qual a carga horária: ");
+        int cargaHorariaEditada = scanner.nextInt();
+        scanner.nextLine();
+
+        cadastro.add(new Cadastro(campusEditadoCurso, disciplinaEditadaCurso, turnoEditadoCurso, cargaHorariaEditada));
+
+        System.out.println("========= Curso editado com Sucesso!===========");
     }
 
     public int getCargaHoraria() {
@@ -185,7 +156,6 @@ public class Curso {
 
     public void setCampusAcademico(String campusAcademico) {
         this.campusAcademico = campusAcademico;
-
     }
 
     private class Cadastro {
@@ -193,13 +163,13 @@ public class Curso {
         private String campus;
         private String disciplina;
         private String turno;
-        private int CargaHoraria;
+        private int cargaHoraria;
 
-        public Cadastro(String campus, String disciplina, String turno, int CargaHoraria) {
+        public Cadastro(String campus, String disciplina, String turno, int cargaHoraria) {
             this.campus = campus;
             this.disciplina = disciplina;
             this.turno = turno;
-            this.CargaHoraria = CargaHoraria;
+            this.cargaHoraria = cargaHoraria;
         }
 
         public String getCampus() {
@@ -227,13 +197,13 @@ public class Curso {
         }
 
         public int getCargaHoraria() {
-            return CargaHoraria;
+            return cargaHoraria;
         }
 
-        public void setCargaHoraria(int CargaHoraria) {
-            this.CargaHoraria = CargaHoraria;
+        public void setCargaHoraria(int cargaHoraria) {
+            this.cargaHoraria = cargaHoraria;
         }
 
     }
-
 }
+
