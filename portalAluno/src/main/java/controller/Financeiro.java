04884133046451db08648fa94acc3e6dcaac7cf7
BrunoStefanello <126917468+BrunoStefanello@users.noreply.github.com>
2023-06-14 20:21:23 -0300

    Update Financeiro.java
    Atualização codigo

--- portalAluno/src/main/java/controller/Financeiro.java
+++ portalAluno/src/main/java/controller/Financeiro.java
@@ -4,87 +4,82 @@ Analista Responsável:  Renan
 Desenvolvedor: Bruno
  */
 package controller;
-
+import java.util.*;
 public class Financeiro {
-
+    
     private int numMensalidade;
     public double mensalidade;
     public int pagamento;
     private int notaFiscal;
 
+    ArrayList mensalidades = new ArrayList();
+    ArrayList cartoes = new ArrayList();
+    ArrayList notasFiscais = new ArrayList();
+    
     public static void financeiro(String[] args) {
         System.out.println("Em Desenvolvimento");
         Menu.main(args);
     }
-
-    public void realizarPagamento() {
-
+    
+    public void realizarPagamento(int pNumMensalidade){
+        for (Object mensalidade : mensalidades) {
+            System.out.println("Você pagou esta mensalidade");
+        }
     }
-
-    public void cadastrarCartaoCredito() {
-
+    
+    public void cadastrarCartaoCredito(int pNumCartaoCredito) {
+        for (Object cartao : cartoes) {
+            System.out.println("Informe o numero do cartão que deseja adicionar");
+            cartoes.add(pNumCartaoCredito);
+    
+        }
     }
-
-    private void gerarNotaFiscal() {
-
+    
+    public void gerarNotaFiscal(int pQtdNota){
+        for (int i = 0; i < pQtdNota; i++) {
+         double randomNumber = Math.random();    
+        notasFiscais.add(randomNumber);
+        }
+        
     }
-
-    public void emitirGerarNotaFiscal() {
-
+    
+    public void emitirGerarNotaFiscal(int pNumNotaFiscal){
+        notasFiscais.get(pNumNotaFiscal);
     }
-
-    public void exibirBoleto() {
-
+    
+    public void removerCartaoCredito(int pNumCartao) {
+        cartoes.remove(pNumCartao);
+  
     }
-
-    public void salvarPagamento() {
-
-    }
-
-    public void removerCartaoCredito() {
-
-    }
-
-    private void gerarQrCode() {
-
-    }
-
-    public void exibirQrCode() {
-
-    }
-
-    public void removerPagamento() {
-
-    }
-
-    public void setNotaFiscal(int notaFis) {
+    
+    public void setNotaFiscal(int notaFis){
         this.notaFiscal = notaFis;
     }
-
-    public int getNotaFiscal() {
+    
+    public int getNotaFiscal(){
         return this.notaFiscal;
     }
-
-    public void setMensalidade(double mensal) {
+    
+    public void setMensalidade(double mensal){
         this.mensalidade = mensal;
     }
-
+    
     public double getMensalidade() {
         return this.mensalidade;
     }
-
-    public void setPagamento(int pag) {
+    
+    public void setPagamento(int pag){
         this.pagamento = pag;
     }
-
+    
     public int getPagamento() {
         return this.pagamento;
     }
-
-    public void setNumMensalidade(int numMen) {
+    
+    public void setNumMensalidade(int numMen){
         this.numMensalidade = numMen;
     }
-
+    
     public int setNumMensalidade() {
         return this.numMensalidade;
     }
