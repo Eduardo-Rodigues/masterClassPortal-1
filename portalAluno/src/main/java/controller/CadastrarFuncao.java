f576b7a89543389805678133d93d9d07fc737743
Eduardo Rodrigues <edurodrigues188@gmail.com>
2023-06-14 19:17:43 -0300

    Create CadastrarFuncao.java
    Criação da classe para cadastrar Funcao

---
+++ portalAluno/src/main/java/controller/CadastrarFuncao.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author eduro
+ */
+public class CadastrarFuncao {
+    private RepositorioFuncao repository;
+    
+    public CadastrarFuncao(RepositorioFuncao repository) {
+        this.repository = repository;
+    }
+    
+    public void cadastrarFuncao() {
+        Scanner read = new Scanner(System.in).useLocale(new Locale("pt", "BR"));
+        System.out.print("Digite o nome da função: ");
+        String nome = read.nextLine();
+        repository.adicionarFuncao(nome);
+        System.out.println("Função cadastrada com sucesso!");
+    }
+
+}
