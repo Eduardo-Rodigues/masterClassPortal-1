d574566811a9f2d7473f59670e5eb52346b7b508
Eduardo Rodrigues <edurodrigues188@gmail.com>
2023-06-14 19:17:19 -0300

    Update MenuFuncao.java
    Alteração do menu de seleção.

--- portalAluno/src/main/java/controller/MenuFuncao.java
+++ portalAluno/src/main/java/controller/MenuFuncao.java
@@ -4,6 +4,9 @@
  */
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -12,45 +15,55 @@ import java.util.Scanner;
  */
 public class MenuFuncao {
 
-    public static void main(String[] args) {
-        TipoFuncao menuTipoFuncao = new TipoFuncao();
-        Scanner scanner = new Scanner(System.in);
-        int opcao = -1;
-        while (opcao != 0) {
-            System.out.println("Escolha uma opção:\n"
-                    + "1 - Cadastrar função\n"
-                    + "2 - Excluir função\n"
-                    + "3 - Editar função\n"
-                    + "4 - Exibir funções cadastradas\n"
-                    + "0 - Sair\n"
-                    + "Opção:"
-            );
-            try {
-                opcao = Integer.parseInt(scanner.nextLine());
-                switch (opcao) {
-                    case 1:
-                        menuTipoFuncao.cadastrarFuncao();
-                        break;
-                    case 2:
-                        menuTipoFuncao.excluirFuncao();
-                        break;
-                    case 3:
-                        menuTipoFuncao.editarFuncao();
-                        break;
-                    case 4:
-                        System.out.println(menuTipoFuncao.exibirFuncao());
-                        break;
-                    case 0:
-                        System.out.println("Saindo...");
-                        break;
-                    default:
-                        System.out.println("Opção inválida!");
-                        break;
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Opção inválida!");
+    private RepositorioFuncao repositorio;
+    private CadastrarFuncao cadastro;
+    private EditarFuncao edicao;
+    private ExcluirFuncao exclusao;
+    private ExibirFuncao exibicao;
+    
+    public MenuFuncao() {
+        repositorio = new FuncaoRepositorioImpl();
+        cadastro = new CadastrarFuncao(repositorio);
+        edicao = new EditarFuncao(repositorio);
+        exclusao = new ExcluirFuncao(repositorio);
+        exibicao = new ExibirFuncao(repositorio);
+    }
+    
+    public void iniciar() {
+        Scanner lerInput = new Scanner(System.in);
+        int opcao = 0;
+        
+        while (opcao != 5) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Cadastrar função");
+            System.out.println("2 - Editar função");
+            System.out.println("3 - Excluir função");
+            System.out.println("4 - Exibir funções");
+            System.out.println("5 - Sair");
+            
+            opcao = lerInput.nextInt();
+            lerInput.nextLine(); // Consumir a quebra de linha
+            
+            switch (opcao) {
+                case 1:
+                    cadastro.cadastrarFuncao();
+                    break;
+                case 2:
+                    edicao.editarFuncao();
+                    break;
+                case 3:
+                    exclusao.excluirFuncao();
+                    break;
+                case 4:
+                    System.out.println(exibicao.exibirFuncao());
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
             }
-
         }
     }
 }
