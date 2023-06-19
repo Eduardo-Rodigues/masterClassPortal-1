8747dc2517d0dfb5c3fdf14e85c5a3e0dd3d9fb4
Erick Daniel Teixeira Vier <126730119+ErickDaniel7@users.noreply.github.com>
2023-06-18 21:32:53 -0300

    FormaPagamento.java

--- portalAluno/src/main/java/controller/FormaPagamento.java
+++ portalAluno/src/main/java/controller/FormaPagamento.java
@@ -2,16 +2,6 @@ package controller;
 
 public class FormaPagamento {
     
-
-    private int numeroPagamento;
-    public int pix;
-    public int boleto;
-    public int cartao;
-    public double dinheiro;
-    public double cheque;
-    public double deposito;
-    public double transferencia;
-
     public static void main(String[] args) {
         System.out.println("Em Desenvolvimento!");
     }
